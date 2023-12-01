422b06b8d1dcb3f050d7c8bb4a31034937422308
pipeleon <oflg94@gmail.com>
2023-12-01 15:01:11 -0500

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   integrations: [tailwind(), react(), sitemap()],
   site: 'https://lucent-bunny-643535.netlify.app',
-  /* build: {
-    assetsPrefix: 'https://lucent-bunny-643535.netlify.app'
-  } */
+  build: {
+    assetsPrefix: process.env.PREFIX_ASSETS === 'yes' ? 'https://lucent-bunny-643535.netlify.app' : ''
+  }
 });
