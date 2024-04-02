43e76dc67de859a1915e6654c0169d7a1e3b32b3
David Daza <03dazal@gmail.com>
2024-04-02 10:33:32 -0500

    Update outDir config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-import sitemap from "@astrojs/sitemap";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), sitemap()],
-  site: 'https://lucent-bunny-643535.netlify.app',
+  integrations: [tailwind(), react()],
+  site: 'https://ousroi.datasketch.co/passion-works',
+  outDir: './dist/passion-works',
+  base: '/passion-works',
   build: {
-    assetsPrefix: process.env.PREFIX_ASSETS === 'yes' ? 'https://lucent-bunny-643535.netlify.app' : ''
+    assetsPrefix: process.env.PREFIX_ASSETS === 'yes' ? 'https://ousroi.datasketch.co/passion-works' : ''
   }
 });
