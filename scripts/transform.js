9b11e286f8571b4b5bc780a3879c884836c117f0
David Daza <03dazal@gmail.com>
2024-07-29 17:28:17 -0500

    add circleci config

---
+++ scripts/transform.js
@@ -0,0 +1,33 @@
+// recuperar datos desde s3
+import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+import { writeFile } from 'fs/promises'
+import path from 'path'
+
+const client = new S3Client({
+  region: 'us-east-1',
+  credentials: {
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY
+  },
+});
+
+async function getObject(key) {
+  const command = new GetObjectCommand({
+    Bucket: 'uploads.dskt.ch',
+    Key: key,
+  });
+
+  const { Body } = await client.send(command);
+
+  const contents = await Body?.transformToString();
+  const data = JSON.parse(contents || "") || [];
+
+  return data
+}
+
+const themes = await getObject('ddazal/passion-works/themes.json')
+
+// TODO: transformar a template
+
+// Guardar en filesystem
+await writeFile(path.join(process.cwd(), 'public', 'data', 'format_.json'), JSON.stringify(themes))
