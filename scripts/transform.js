7b7a54660014d9afbfef37a1fb1b520bf409173f
pipeleon <oflg94@gmail.com>
2024-07-31 11:51:16 -0500

    refactor script

--- scripts/transform.js
+++ scripts/transform.js
@@ -3,6 +3,7 @@ import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
 import { writeFile } from 'fs/promises'
 import path from 'path'
 import omit from "lodash.omit";
+import groupBy from "lodash.groupby";
 
 const client = new S3Client({
   region: 'us-east-1',
@@ -27,8 +28,6 @@ async function getObject(key) {
 }
 
 async function main() {
-  /* const themes = await getObject(`ddazal/${process.env.PROXY_DATABASE}/list_references.json`) */
-  const data = {}
   const [themes, proxys, references, proxy_inputs, prooxy_value] = await Promise.all([
     getObject(`${process.env.ORGANIZATION_SLUG}/${process.env.PROXY_DATABASE}/themes.json`),
     getObject(`${process.env.ORGANIZATION_SLUG}/${process.env.PROXY_DATABASE}/proxies.json`),
@@ -36,13 +35,14 @@ async function main() {
     getObject(`${process.env.ORGANIZATION_SLUG}/${process.env.PROXY_DATABASE}/proxy_inputs.json`),
     getObject(`${process.env.ORGANIZATION_SLUG}/${process.env.PROXY_DATABASE}/proxy_values.json`)
   ])
+
+  const data = {}
   data.general = {}
-  data.tabs = []
-  data.proxy_inputs = proxy_inputs.map(item => { return { ...omit(item, ["rcd___id"]), changed: false } })
-  data.proxy_values = prooxy_value.map(item => { return { ...omit(item, ["rcd___id"]), changed: false } })
+  data.proxy_inputs = proxy_inputs.map(item => ({ ...omit(item, ["rcd___id"]), changed: false }))
+  data.proxy_values = prooxy_value.map(item => ({ ...omit(item, ["rcd___id"]), changed: false }))
 
   // TODO: transformar a template
-  let counter = true
+  /* let counter = true
   let counter2 = true
   let tmp = {}
   let tmp2 = {}
@@ -83,9 +83,48 @@ async function main() {
         }
       }
     }
-  }))
+  })) */
+
+  // General
+  const generalSection = themes.filter(row => row.section.trim() === 'general')
+  generalSection.forEach(row => {
+    data.general[row.variable] = row.value
+  })
+
+
+  // Tabs
+  const tabs = themes.filter(item => item.section.trim().split('/').length === 2)
+
+  const groupedBySection = groupBy(tabs, 'section')
+
+  data.tabs = Object.keys(groupedBySection).map(sectionKey => {
+    return groupedBySection[sectionKey].reduce((accumulator, currentItem) => {
+      accumulator[currentItem.variable] = currentItem.value
+      return accumulator
+    }, {
+      tables: [],
+      list: sectionKey === 'tabs/references' ? references.map(item => omit(item, ["rcd___id"])) : []
+    })
+  })
+
+  // Tables
+  const tables = themes.filter(item => item.section.split('/').length === 4)
+
+  const groupByTables = groupBy(tables, 'section')
+
+  data.tabs.find(tab => tab.type === 'table').tables = Object.keys(groupByTables).map(tableKey => {
+    return groupByTables[tableKey].reduce((accumulator, currentItem) => {
+      accumulator[currentItem.variable] = currentItem.value
+      return accumulator
+    }, {
+      changed: false,
+      rows: proxys.map(item => ({ ...omit(item, ["rcd___id"]), changed: false })).filter(item => item.type === tableKey.split('/')[3] + '_impact')
+    })
+  })
+
+
   // Guardar en filesystem
-  await writeFile(path.join(process.cwd(), 'public', 'data', 'test.json'), JSON.stringify(data))
+  await writeFile(path.join(process.cwd(), 'public', 'data', 'format.json'), JSON.stringify(data))
 }
 
 main().catch(err => {
