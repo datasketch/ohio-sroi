b59b8d98571a05a281ebb2041bead6afd4cee714
pipeleon <oflg94@gmail.com>
2024-07-31 09:32:12 -0500

    adding script to transform raw data to pages jason

--- scripts/transform.js
+++ scripts/transform.js
@@ -2,6 +2,7 @@
 import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
 import { writeFile } from 'fs/promises'
 import path from 'path'
+import omit from "lodash.omit";
 
 const client = new S3Client({
   region: 'us-east-1',
@@ -25,9 +26,69 @@ async function getObject(key) {
   return data
 }
 
-const themes = await getObject(`${process.env.ORGANIZATION_SLUG}/${process.env.PROXY_DATABASE}/themes.json`)
+async function main() {
+  /* const themes = await getObject(`ddazal/${process.env.PROXY_DATABASE}/list_references.json`) */
+  const data = {}
+  const [themes, proxys, references, proxy_inputs, prooxy_value] = await Promise.all([
+    getObject(`${process.env.ORGANIZATION_SLUG}/${process.env.PROXY_DATABASE}/themes.json`),
+    getObject(`${process.env.ORGANIZATION_SLUG}/${process.env.PROXY_DATABASE}/proxies.json`),
+    getObject(`${process.env.ORGANIZATION_SLUG}/${process.env.PROXY_DATABASE}/list_references.json`),
+    getObject(`${process.env.ORGANIZATION_SLUG}/${process.env.PROXY_DATABASE}/proxy_inputs.json`),
+    getObject(`${process.env.ORGANIZATION_SLUG}/${process.env.PROXY_DATABASE}/proxy_values.json`)
+  ])
+  data.general = {}
+  data.tabs = []
+  data.proxy_inputs = proxy_inputs.map(item => { return { ...omit(item, ["rcd___id"]), changed: false } })
+  data.proxy_values = prooxy_value.map(item => { return { ...omit(item, ["rcd___id"]), changed: false } })
 
-// TODO: transformar a template
+  // TODO: transformar a template
+  let counter = true
+  let counter2 = true
+  let tmp = {}
+  let tmp2 = {}
+  let tab = ''
+  themes.map(((item, i) => {
+    if (item.section === 'general') {
+      data.general[item.variable] = item.value
+    } else {
+      const lengt_tab = item.section.split('/')
+      if (lengt_tab.length === 2) {
+        if (counter) {
+          tmp = {}
+          tmp[item.variable] = item.value
+          counter = false
+        } else {
+          tmp[item.variable] = item.value
+          tmp.tables = []
+          tmp.list = item.value === 'references' ? references : []
+          data.tabs.push(tmp)
+          counter = true
+        }
+      } else {
+        if (counter2) {
+          tmp2 = {}
+          tab = lengt_tab[3]
+          tmp2[item.variable] = item.value
+          counter2 = false
+        } else {
+          if (tab === themes[i + 1].section.split('/')[3]) {
+            tmp2[item.variable] = item.value
+          } else {
+            tmp2[item.variable] = item.value
+            tmp2.changed = false
+            tmp2.rows = proxys.filter(item => item.type === tab + '_impact')
+            data.tabs[0].tables.push(tmp2)
+            counter2 = true
+          }
+        }
+      }
+    }
+  }))
+  // Guardar en filesystem
+  await writeFile(path.join(process.cwd(), 'public', 'data', 'test.json'), JSON.stringify(data))
+}
 
-// Guardar en filesystem
-await writeFile(path.join(process.cwd(), 'public', 'data', 'format_.json'), JSON.stringify(themes))
+main().catch(err => {
+  console.error(err);
+  process.exit(1)
+})
